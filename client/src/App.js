c386d64befada901f75aba4fefd0f68652fef8d9
Max Kottong <max.kottong@gmail.com>
2023-02-15 20:48:40 -0500

    Added MUI and footer, fixed file structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Test from "./components/testComponent";
+import Footer from "./components/footer";
+import './App.css';
+
+function App() {
+    return (
+        <div className="flex-column justify-flex-start min-100-vh container">
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
