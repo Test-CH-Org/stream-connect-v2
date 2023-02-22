a52dd66e83895bf116c3929303f3fa441b5d4609
Max Kottong <max.kottong@gmail.com>
2023-02-22 18:16:06 -0500

    Added mui text color/centering

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
 
 const Footer = () => {
     let copy = String.fromCodePoint(0x00A9);
 
     return (
         <footer
-            className="w-100 mt-auto p-4 text-center footer flex-row
-            justify-content-center"
+            className="mui--text-light"
         >
             <div className="container footer">
-                Get copyrighted {copy}
+                <Typography color="common.black" align="center">
+                    Get copyrighted {copy}
+                </Typography>
             </div>
         </footer>
     )
