177a68bdab1f22f15ff87d071d7711e74a5e82b7
Brandon Davis <brancdavis@gmail.com>
2023-02-22 20:52:18 -0500

    added searchLog model and refs

---
+++ server/models/SearchLog.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class SearchLog extends Model { }
+
+SearchLog.init({
+    id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false,
+    },
+    user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "user",
+				key: "id",
+			},
+    },
+    search: {
+        //Longest movie name ever:
+        //Night of the Day of the Dawn of the Son of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Alien, Flesh Eating, Hellbound, Zombified Living Dead Part 2
+        type: DataTypes.STRING(256),
+        allowNull: false,
+    }
+    
+});
+
+module.exports = SearchLog;
+
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,15 @@
 const User = require("./User");
+const SearchLog = require("./SearchLog");
+
+// user can have many items
+User.hasMany(SearchLog, {
+  foreignKey: "user_id",
+});
+
+// each item with unique description belongs to one user
+SearchLog.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 
-
-module.exports = { User };
+module.exports = { User, SearchLog };
