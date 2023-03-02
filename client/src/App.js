c9c92c76fef6b53400b2f0d4e70bbe7ed6818a55
Max Kottong <max.kottong@gmail.com>
2023-03-01 20:43:02 -0500

    Added SearchResults component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Test from "./components/testComponent";
 import Footer from "./components/footer";
+import SearchResults from "./components/searchResults";
 
 import { Box } from '@mui/material';
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
     return (
         <Box minHeight="100vh" className="flex-column justify-flex-start min-100-vh container">
+            <SearchResults />
             <Footer />
         </Box>
     );
---
+++ client/src/components/searchResults/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+const searchResults = () => {
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
+
+export default searchResults;
