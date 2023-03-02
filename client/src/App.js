d4e9d47ec91faf557ddd84f8fde9526690a0fa4c
Hannahbird <hannahbrownson18@gmail.com>
2023-03-01 19:45:09 -0500

    basic header tag created and logo svg implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import Test from "./components/testComponent";
 import Footer from "./components/footer";
+import Header from "./components/header";
+import Logo from "./components/logo";
 import './App.css';
 
 function App() {
     return (
+       
         <div className="flex-column justify-flex-start min-100-vh container">
+             <Header><Logo/></Header>
             <Footer />
-        </div>
+            </div>
+            
+        
+        
     );
 }
 
