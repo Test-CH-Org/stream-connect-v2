7b1e9ecdebbfc48b80ef767bbe1a7bcec7df6421
Brandon Davis <brancdavis@gmail.com>
2023-03-01 20:14:36 -0500

    updates to models and user APIs

--- server/models/SearchLog.js
+++ server/models/SearchLog.js
@@ -23,7 +23,30 @@ SearchLog.init({
         //Night of the Day of the Dawn of the Son of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Alien, Flesh Eating, Hellbound, Zombified Living Dead Part 2
         type: DataTypes.STRING(256),
         allowNull: false,
-    }},
+    },
+    isActor: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    providerFilter: {
+        type: DataTypes.STRING,
+        get() {
+            return this.getDataValue('providerFilter').split(',');
+        },
+        set(val) {
+            this.setDataValue('providerFilter', val.join(','));
+        }
+    },
+    movieRating: {
+        type: DataTypes.STRING,
+        get() {
+            return this.getDataValue('movieRating').split(',');
+        },
+        set(val) {
+            this.setDataValue('movieRating', val.join(','));
+        }
+    }
+    },
     {
     sequelize,
     timestamps: true,
--- server/server.js
+++ server/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // import sequelize connection
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 6000000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
