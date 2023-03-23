df0081c5fb254349343427e58be2c7c603a35a8f
Hannahbird <hannahbrownson18@gmail.com>
2023-03-22 22:01:06 -0400

    header updated with appbar

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { Box, Typography } from '@mui/material/';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "../logo/index";
 
-const Header = () => {
-
-    return (
-        <header>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                py: 2,
-                bgcolor: '#11171E',
-                width: '100%',
-                position: 'absolute'
-            }}
-                className="container header"
-            >
-
-                <Typography color="#fff" align="center">
-                    <Logo/>
-                </Typography>
-            </Box>
-        </header>
-    )
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }} backgroundColor="black">
+      <AppBar position="static" style={{backgroundColor:"black"}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography color="#fff" align="center" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Logo />
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header;
